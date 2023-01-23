bab224a56f02ed9a317b425444d2182f4c529298
sykes-9 <sykessackitey9@gmail.com>
2023-01-23 08:41:49 +0000

    a simple one page react tailwindcss

---
+++ src/components/Support.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import{ PhoneIcon,ArrowSmallRightIcon } from '@heroicons/react/24/outline'
+import { CpuChipIcon, Square2StackIcon } from '@heroicons/react/20/solid'
+
+import supportImg from '../assets/support.jpg'
+
+const Support = () => {
+  return (
+    <div className='w-full mt-24'>
+        <div className='w-full h-[700px] bg-gray-900/90 absolute'>
+          <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="" />  
+        </div>
+
+        <div className='max-w-[1240px] mx-auto text-white relative'>
+
+            <div className='px-4 py-12'>
+                <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Support</h2>
+                <h3 className='text-5xl font-bold py-6 text-center'>Finding the right team</h3>
+                <p className='py-4 text-3xl text-slate-300'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque dolores magnam recusandae. Id expedita soluta voluptatem libero sed, rerum debitis. Consequuntur, dignissimos mollitia! A recusandae molestias exercitationem iusto? Consequuntur, nulla.</p>
+            </div>
+
+            <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+
+               <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                    <PhoneIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+                    <h3 className='font-bold text-2xl my-6'>Sales</h3>
+                    <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis libero, nemo assumenda ullam sed corporis odit rem eveniet aliquid minus! Iure iste obcaecati laudantium expedita dolores asperiores eos dolorum ipsa!</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                    <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+                  </div>
+               </div>
+
+               <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                    <Square2StackIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+                    <h3 className='font-bold text-2xl my-6'>Technical Support</h3>
+                    <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis libero, nemo assumenda ullam sed corporis odit rem eveniet aliquid minus! Iure iste obcaecati laudantium expedita dolores asperiores eos dolorum ipsa!</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                    <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+                  </div>
+               </div>
+
+               <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                    <CpuChipIcon className='w-16 p-4 bg-indigo-600 text-white rounded-lg mt-[-4rem]'/>
+                    <h3 className='font-bold text-2xl my-6'>Media Inquiries</h3>
+                    <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis libero, nemo assumenda ullam sed corporis odit rem eveniet aliquid minus! Iure iste obcaecati laudantium expedita dolores asperiores eos dolorum ipsa!</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                    <p className='flex items-center text-indigo-600'>Contact Us <ArrowSmallRightIcon className='w-5 ml-2'/></p>
+                  </div>
+               </div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Support
